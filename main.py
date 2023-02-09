0dac331fc760e053aa90c0393b25ba31f6dc57a6
ssidders <sarahsidders@bpp.com>
2023-02-09 16:06:02 +0000

    Removed repeating code

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+def get_tasks(filepath="tasks.txt"):
+    """ Read a text file and return the list of task items."""
+    with open(filepath, 'r') as file:
+        tasks = file.readlines()
+    return tasks
+
+def update_tasks(tasks, filepath="tasks.txt"):
+    """ Write task items into a text file."""
+    with open(filepath, 'w') as file:
+        tasks = file.writelines(tasks)
+
+
 while True:
     user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.strip()
@@ -5,17 +17,14 @@ while True:
     if user_action.startswith('add'):
         task = user_action[4:]
 
-        with open('tasks.txt', 'r') as file:
-            tasks = file.readlines()
-
+        tasks = get_tasks()
         tasks.append(task + '\n')
 
-        with open('tasks.txt', 'w') as file:
-            tasks = file.writelines(tasks)
+        update_tasks(tasks)
 
     elif user_action.startswith('show'):
-        with open('tasks.txt', 'r') as file:
-            tasks = file.readlines()
+        
+        tasks = get_tasks()
 
         for index, item in enumerate(tasks):
             item = item.strip('\n')
@@ -28,14 +37,13 @@ while True:
             print(number)
             number = number - 1
 
-            with open('tasks.txt', 'r') as file:
-                tasks = file.readlines()
+            tasks = get_tasks()
 
             new_task = input ("Enter new task: ")
             tasks[number] = new_task + '\n'
 
-            with open('tasks.txt', 'w') as file:
-                tasks = file.writelines(tasks)
+            update_tasks(tasks)
+
         except ValueError:
             print("Your command is not valid")
             continue
@@ -44,22 +52,19 @@ while True:
         try:
             number = int(user_action[9:])
 
-            with open('tasks.txt', 'r') as file:
-                tasks = file.readlines()
-
+            tasks = get_tasks()
             index = number - 1
             task_to_remove = tasks[index].strip('\n')
             tasks.pop(index)
 
-            with open('tasks.txt', 'w') as file:
-                tasks = file.writelines(tasks)
+            update_tasks(tasks)
 
             message = f"Task: {task_to_remove}, was removed from the list." 
             print(message)   
-        except: IndexError
-        print("There is no item with that number")    
-        continue
-
+        except IndexError:
+            print("There is no item with that number")    
+            continue
+        
     elif 'exit' in user_action:
         break
 
